756b79cfe795fa0e98322bb02fb30e786766c10e
evaughn@nd.edu <evaughn@nd.edu@48746f98-36a5-0e54-d474-7dbab5672a21>
2012-03-15 01:54:37 +0000

    - tried to make indents consistent - optimized Deck class a lot, cleaned it up, rewrote the newDeck function - corrected a logical error in Board.cpp - removed more unused includes.
    I'll probably work on moving cards from the playfield to the top 4 ending zones.  Also to do is making options for dealing (3card, 1card), etc.

--- Board.h
+++ Board.h
@@ -29,18 +29,18 @@ using namespace std;
 class Board{
 
 public:
-Board();
-void deal();			//deals out cards
-void print();			//prints out all the columns
-int isAllowed(int, int); 	//returns 1 if it's allowed 
-int cardNumber(const int); 	//returns number of card (0 = Ace, 12=K)
-int suitNum(const int);		//returns 1 for red, 0 for black
-CardColumn moveCards(CardColumn, CardColumn); 	//copies first parameter cardcolumn to the end of the second cardcolumn
-CardColumn getColumn(int);	//returns cardcolumn 
-void setColumn(CardColumn,int);	//sets cardcolumn # int
+	Board();
+	void deal();			//deals out cards
+	void print();			//prints out all the columns
+	int isAllowed(int, int); 	//returns 1 if it's allowed 
+	int cardNumber(const int); 	//returns number of card (0 = Ace, 12=K)
+	int suitNum(const int);		//returns 1 for red, 0 for black
+	CardColumn moveCards(CardColumn, CardColumn); 	//copies first parameter cardcolumn to the end of the second cardcolumn
+	CardColumn getColumn(int);	//returns cardcolumn 
+	void setColumn(CardColumn,int);	//sets cardcolumn # int
 private: 
-CardColumn column[20];
-CardDeck deck;
+	CardColumn column[20];
+	CardDeck deck;
 };
 #endif
 
